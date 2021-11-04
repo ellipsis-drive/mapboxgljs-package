721ae9886caac6c8a26bff74ef79695422fc5e1e
Jelle <jelle.keulemans@gmail.com>
2021-11-04 22:47:43 +0100

    gave source a unique id

--- src/lib/EllipsisRasterLayer.js
+++ src/lib/EllipsisRasterLayer.js
@@ -2,16 +2,16 @@ class EllipsisRasterLayer {
 
     constructor(blockId, captureId, visualizationId, maxZoom = 18, token) {
         this.id = `${blockId}_${captureId}_${visualizationId}`;
-        this.source = this.id;
+        this.source = `${this.id}_source`;
         this.type = 'raster';
         this.url = `${EllipsisApi.apiUrl}/tileService/${blockId}/${captureId}/${visualizationId}/{z}/{x}/{y}`;
         if (token) {
             url += '?token=' + token;
         }
-        
     }
+
     addTo(map) {
-        map.addSource(this.id, {
+        map.addSource(this.source, {
             type: 'raster',
             tiles: [
                 this.url
@@ -20,6 +20,7 @@ class EllipsisRasterLayer {
         });
 
         map.addLayer(this);
+        return this;
     }
 }
 
